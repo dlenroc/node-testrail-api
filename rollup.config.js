2af18a671dfd5ca1adf19ccc801901d8455baef5
Corneliu Duplachi <cornel.duplachi@gmail.com>
2020-12-02 01:07:27 +0200

    feat(node): reuse TCP connection

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,11 @@
-import pkg from './package.json';
-import {terser} from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
 
 export default {
   input: 'src/TestRail.ts',
   external: [],
   output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'default',
-      strict: false,
-      sourcemap: true,
-      banner: `'use strict';\n`
-        + `const fetch = require('node-fetch');\n`
-        + `const FormData = require('form-data');\n`
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'default',
-      sourcemap: true,
-      banner: ``
-        + `import fetch from 'node-fetch';\n`
-        + `import FormData from 'form-data';\n`
-    },
     {
       file: pkg.browser,
       format: 'es',
@@ -37,10 +18,43 @@ export default {
       exports: 'default',
       sourcemap: true,
       name: 'TestRail'
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'default',
+      strict: false,
+      sourcemap: true,
+      banner: `'use strict';\n`
+        + `const http = require('http');\n`
+        + `const https = require('https');\n`
+        + `const nodeFetch = require('node-fetch');\n`
+        + `const FormData = require('form-data');\n\n`
+        + `const options = { keepAlive: true };\n`
+        + `const httpAgent = new http.Agent(options);\n`
+        + `const httpsAgent = new https.Agent(options);\n`
+        + `const agent = (url) => url.protocol === 'http:' ? httpAgent : httpsAgent;\n`
+        + `const fetch = (url, init) => nodeFetch(url, { agent, ...init });\n`,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'default',
+      sourcemap: true,
+      banner: ``
+        + `import http from 'http';\n`
+        + `import https from 'https';\n`
+        + `import nodeFetch from 'node-fetch';\n`
+        + `import FormData from 'form-data';\n\n`
+        + `const options = { keepAlive: true };\n`
+        + `const httpAgent = new http.Agent(options);\n`
+        + `const httpsAgent = new https.Agent(options);\n`
+        + `const agent = (url) => url.protocol === 'http:' ? httpAgent : httpsAgent;\n`
+        + `const fetch = (url, init) => nodeFetch(url, { agent, ...init });\n`,
     }
   ],
   plugins: [
-    typescript({useTsconfigDeclarationDir: true}),
+    typescript({ useTsconfigDeclarationDir: true }),
     terser()
   ]
 }
